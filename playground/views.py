c528b877262b999e7d12a593800105db4b320c8b
DeepakKumarDev <deepak.dev.mca22.du@gmail.com>
2024-04-17 15:42:08 +0530

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse 
+
+# def say_hello(request):
+#     return HttpResponse('Hello Deepak  ')
+
+
+def say_hello(request):
+    return render(request,'hello.html',{'name':"Deepak"})
